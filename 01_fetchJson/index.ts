5ba4463607ec7fc24c807215c4fce220bd4536f3
Levi <levi-sr@outlook.com>
2020-10-28 23:36:00 -0300

    prevent some basic errors with JS

--- 01_fetchJson/index.ts
+++ 01_fetchJson/index.ts
@@ -2,6 +2,31 @@ import axios from 'axios';
 
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 
+//used to define a structure of an object
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
 axios.get(url).then((response) => {
-  console.log(response.data);
+  //Response.data has properties of
+  //id
+  //title
+  //completed
+  const todo = response.data as Todo;
+
+  console.log(todo);
+
+  const { id, title, completed } = todo;
+
+  logTodo(id, title, completed);
 });
+
+const logTodo = (id: number, title: string, completed: boolean) => {
+  console.log(`
+  The todo with id : ${id}
+  has a title of: ${title}
+  Is it finished? ${completed}
+`);
+};
